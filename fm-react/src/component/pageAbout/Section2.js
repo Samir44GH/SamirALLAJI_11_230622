34f5cdbdc85ced3ad52917702edef71b7488057b
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-09-23 16:22:06 +0200

    modif final

--- fm-react/src/component/pageAbout/Section2.js
+++ fm-react/src/component/pageAbout/Section2.js
@@ -3,12 +3,13 @@ import React from "react";
 const Section2 = () => {
   return (
     <section className="section2">
-      <p alt=" Photo_de_montagne " className="imgAbout"></p>
-      {/* <img
+      
+       <img
         src="./images/imgAbout.png"
+    srcSet="./images/AP1440.jpg 767.9w 1023.9w 1439.9w"
         alt=" Photo_de_montagne "
         className="imgAbout"
-      /> */}
+      /> 
     </section>
   );
 };

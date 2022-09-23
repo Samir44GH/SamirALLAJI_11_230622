5551e37a779e74d08b49db3dded5552e2294b167
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-09-23 16:24:47 +0200

    modif final

--- fm-react/src/component/pageHome/Body1.js
+++ fm-react/src/component/pageHome/Body1.js
@@ -3,12 +3,13 @@ import React from "react";
 const Body1 = () => {
   return (
     <section className="sectionSlogan">
-      <p className="sloganImg" alt="Falaises au bord de la mer"></p>
-      {/* <img
+      
+       <img
         // src="./images/IMGboody1-1.jpg"
+    srcSet="./images/IMGo.png 767.9w 1023.9w 1439.9w "
         className="sloganImg"
         alt="Falaises au bord de la mer"
-      /> */}
+      /> 
       <h1 className="sloganTitle">Chez vous, partout et ailleurs</h1>
     </section>
   );

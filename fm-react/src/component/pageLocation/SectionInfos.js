831a57646e1fe41be2e05e61bebdf4e1024ec807
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-10-03 16:27:35 +0200

    modif ajout,
     import DDAbout.js suppression de dropDowwn.js DropDown2.js

--- fm-react/src/component/pageLocation/SectionInfos.js
+++ fm-react/src/component/pageLocation/SectionInfos.js
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import InfosLogement from "./InfosLogement";
 import Stars from "./Stars";
 import Host from "./Host";
-import Dropdown from "./DropDown";
-import Dropdown2 from "./DropDown2";
+import DDabout from "../pageAbout/DDabout";
 
 const SectionInfos = (props) => {
   useEffect(() => console.log(props.location));
@@ -19,8 +18,8 @@ const SectionInfos = (props) => {
         </div>
       </div>
       <div className="menuDD">
-        <Dropdown title="Description" DD={props.location?.description} />
-        <Dropdown2 title="Equipements" DD={props.location?.equipments} />
+       <DDabout title="Description" dropAbout={props.location.description} />
+        <DDabout title="Equipements" dropAbout={props.location.equipments} />
       </div>
       {/* <Dropdown title="Equipements" DD={props.location?.equipments} /> */}
     </div>

de0c12938423a6fa0c2f657f48b0d00c2c5eb802
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-10-07 16:28:27 +0200

    Modif cardsLocation avec <Link>
    Modification avec <Link> l.1 à l.13 CardLocation.js

--- fm-react/src/component/pageHome/CardsLocation.js
+++ fm-react/src/component/pageHome/CardsLocation.js
@@ -1,33 +1,21 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const CardsLocation = ({ location }) => {
-  console.log(location);
-
-  console.log(useParams());
-  const navigate = useNavigate();
-
-  const goToLocation = (id) => {
-    navigate(`/location/${id}`);
-  };
+const CardsLocation = ({ appart }) => {
+  console.log(appart);
+  console.log(appart.id);
 
   return (
-    <article
-      className="locationCards"
-      onClick={() => {
-        goToLocation(location.id);
-      }}
-      // onClick={data.map((location, index) => (
-      //   <Location key={index} location={location.id} />
-      //   // <h1 key={index.rating}>{location.title}</h1>
-      // ))}
-    >
-      <img src={location.cover} alt={location.title} className="imgCards" />
-
-      <div className="infos">
-        <h2 className="infosTitle">{location.title}</h2>
-      </div>
-    </article>
+    <div>
+      <article className="locationCards" key={appart.id}>
+        <Link to={`/location/${appart.id}`}>
+          <img src={appart.cover} alt={appart.title} className="imgCards" />
+        </Link>
+        <div className="infos">
+          <h2 className="infosTitle">{appart.title}</h2>
+        </div>
+      </article>
+    </div>
   );
 };
 

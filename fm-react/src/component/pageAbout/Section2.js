5cb5c5b5ca2a9296a89d325a1e53c0cceed5415f
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-10-11 16:03:44 +0200

    Modif section2.js

--- fm-react/src/component/pageAbout/Section2.js
+++ fm-react/src/component/pageAbout/Section2.js
@@ -3,13 +3,15 @@ import React from "react";
 const Section2 = () => {
   return (
     <section className="section2">
-      
-       <img
+      <source
+        media="(min-width: 1439.9px)"
+        srcset="./images/AP1440.jpg"
+      ></source>
+      <img
         src="./images/imgAbout.png"
-    srcSet="./images/AP1440.jpg 767.9w 1023.9w 1439.9w"
         alt=" Photo_de_montagne "
         className="imgAbout"
-      /> 
+      />
     </section>
   );
 };

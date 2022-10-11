b5ba7818e414fe14247f11e5c9bddabdf59dbaf3
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-10-11 17:20:55 +0200

    Réduction
    Réduction de deux composants dans un seul composant

--- fm-react/src/component/pageHome/MainHome.js
+++ fm-react/src/component/pageHome/MainHome.js
@@ -1,13 +1,32 @@
-import React from "react";
-import Body1 from "./Body1";
-import Body2 from "./Body2";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CardsLocation from "./CardsLocation";
 
 const MainHome = () => {
+  const [data, setData] = useState([]);
+  //Le UseEffect() se joue lorsque le composant est appelé
+  useEffect(() => {
+    axios.get("./logements.json").then((res) => setData(res.data));
+  }, []);
+
   return (
-    <main className="bodyHome">
-      <Body1 />
-      <Body2 />
-    </main>
+    <div className="bodyHome">
+      <section className="sectionSlogan">
+        <img
+          src="./images/IMGboody1-1.jpg"
+          srcSet="./images/IMGo.png 767.9w 1023.9w 1439.9w "
+          className="sloganImg"
+          alt="Falaises au bord de la mer"
+        />
+        <h1 className="sloganTitle">Chez vous, partout et ailleurs</h1>
+      </section>
+      <section className="locationList">
+        {data.map((location, index) => (
+          <CardsLocation key={index} appart={location} />
+          // <h1 key={index.rating}>{location.title}</h1>
+        ))}
+      </section>
+    </div>
   );
 };
 

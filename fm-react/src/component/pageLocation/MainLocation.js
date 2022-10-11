f3b5f978c44a6a82d33d8996b7325fe6f44a1d99
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-10-11 16:05:15 +0200

    modi mainLocation.js

--- fm-react/src/component/pageLocation/MainLocation.js
+++ fm-react/src/component/pageLocation/MainLocation.js
@@ -7,24 +7,13 @@ import { Navigate } from "react-router";
 
 const MainLocation = () => {
   const [data, setData] = useState([1]);
-  // const [dataLoading, setDataLoading] = useState(false);
   const { id } = useParams();
-
-  // const [tags, setTags] = useState([]);
-  // const [equipments, setEquipments] = useState([]);
-  // const [pictures, setPictures] = useState([]);
-  // const [isDataLoading, setDataLoading] = useState(true);
 
   useEffect(() => {
     console.log(id);
-    // setDataLoading(true);
+
     axios.get("http://localhost:3000/logements.json").then((res) => {
       setData(res.data.filter((location) => location.id === id));
-      // setDataLoading(false);
-      // setTags(data);
-      // setEquipments(res.data.equipments);
-      // setPictures(res.data.pictures);
-
       console.clear();
       console.log(res.data);
     });

3d15d769d0745c9e107cad0136fe5159a589e880
Samir44GH <55440176+Samir44GH@users.noreply.github.com>
2022-10-11 17:32:16 +0200

    Delete commentaire

--- fm-react/src/pages/About.js
+++ fm-react/src/pages/About.js
@@ -2,7 +2,6 @@ import React from "react";
 import Footer from "../component/layout/Footer";
 import Header from "../component/layout/Header";
 import Section from "../component/pageAbout/Section";
-// import Section1 from "../component/pageAbout/Section1";
 import Section2 from "../component/pageAbout/Section2";
 
 const About = () => {
